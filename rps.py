11663ef44b0d8f32793582eb4be5589e0ca623e2
Caitlin Bond <caitlin@kumina.nl>
2021-03-16 22:40:16 +0100

    multiple rounds, game loop, clean up

--- rps.py
+++ rps.py
@@ -2,48 +2,73 @@ import random
 import sys
 
 
-class rsp:
+class RPS:
     def __init__(self):
-        pass
+        self.user_wins = 0
+        self.AI_wins = 0
+        self.first_round = True
+        self.won = False
 
     def play(self):
-        self.input_user()
-        self.ai()
-        self.check()
+        while self.won == False:
+            self.input_user()
+            self.ai()
+            self.check()
+            self.wins()
 
     def input_user(self):
-        print("Hallo welkom a game of rock paper scissors")
-        print("Choose between rock paper or scissors")
+        if self.first_round == True:
+            print("Hallo welcome to a game of rock paper scissors")
+            print("Choose between rock paper or scissors")
+            self.first_round = False
+        else:
+            pass
         self.user_chose = input()
 
     def ai(self):
         rps = list(["rock", "paper", "scissors"])
         self.AI_answer = random.choice(rps)
-        print(self.AI_answer)
+        print("AI", self.AI_answer)
 
     def check(self):
         if self.user_chose == self.AI_answer:
             print("same")
-            sys.exit(0)
         elif self.user_chose == "rock":
             if self.AI_answer == "paper":
-                print("you loss")
+                # AI wins
+                self.AI_wins = self.AI_wins + 1
             elif self.AI_answer == "scissors":
-                print("you win")
+                # user wins
+                self.user_wins = self.user_wins + 1
         elif self.user_chose == "paper":
             if self.AI_answer == "rock":
-                print("you win")
+                # user wins
+                self.user_wins = self.user_wins + 1
             elif self.AI_answer == "scissors":
-                print("you loss")
+                # AI wins
+                self.AI_wins = self.AI_wins + 1
         elif self.user_chose == "scissors":
             if self.AI_answer == "paper":
-                print("you win")
+                # user wins
+                self.user_wins = self.user_wins + 1
             if self.AI_answer == "rock":
-                print("you loss")
+                # AI wins
+                self.AI_wins = self.AI_wins + 1
         else:
             print("I said rock paper or scissors not that shit")
+        print("user wins =", self.user_wins, "AI wins", self.AI_wins)
+
+    def wins(self):
+        if self.user_wins == 3:
+            print("the humans win")
+            self.won = True
+        elif self.AI_wins == 3:
+            print("computers wins and will always be the superior")
+            self.won = True
+        else:
+            pass
 
 
 if __name__ == '__main__':
-    a = rsp()
+    a = RPS()
     a.play()
